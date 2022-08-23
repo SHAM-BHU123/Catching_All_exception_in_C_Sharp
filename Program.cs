07606a950555187087db0f91b03ccc5b06336a86
SAM BHU <SAM BHU@DESKTOP-5MMOUET>
2022-08-23 12:03:38 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Catching_All_exception_in_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            try {
+                int a = 0;
+                int div = 20 / a;
+                Console.WriteLine("I am in try block");
+            }
+
+            catch(Exception)
+            {
+                Console.WriteLine("This block handle all the exceptioin");
+            }
+            finally
+            {
+                Console.WriteLine(" i don't want your job");
+            }
+        }
+    }
+}
